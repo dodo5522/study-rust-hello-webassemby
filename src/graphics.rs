f303e200dab3486aafff5da16592cfc6448cfe71
Takashi Ando <dodo5522@gmail.com>
2025-04-29 08:53:21 +0900

    [main] Draw triangle with Point

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,5 +1,16 @@
 use web_sys::wasm_bindgen::JsCast;
 
+pub struct Point {
+  pub(crate) x: f64,
+  y: f64,
+}
+
+impl Point {
+  pub fn new(x: f64, y: f64) -> Self {
+    Self { x, y }
+  }
+}
+
 pub fn get_canvas_2d(element_id: &str) -> Option<web_sys::CanvasRenderingContext2d> {
   let window = web_sys::window()?;
   let document = window.document()?;
@@ -9,13 +20,22 @@ pub fn get_canvas_2d(element_id: &str) -> Option<web_sys::CanvasRenderingContext
   context.dyn_into::<web_sys::CanvasRenderingContext2d>().ok()
 }
 
-pub fn draw_triangle(context: web_sys::CanvasRenderingContext2d) {
-  context.move_to(200.0, 0.0);
+pub fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: &[Point; 3], fill: bool) {
+  let top = &points[0];
+  let bottom_left = &points[1];
+  let bottom_right = &points[2];
+
+  context.move_to(top.x, top.y);
+
   context.begin_path();
-  context.line_to(0.0, 400.0);
-  context.line_to(400.0, 400.0);
-  context.line_to(200.0, 0.0);
+  context.line_to(bottom_left.x, bottom_left.y);
+  context.line_to(bottom_right.x, bottom_right.y);
+  context.line_to(top.x, top.y);
   context.close_path();
+
   context.stroke();
-  context.fill();
+
+  if fill {
+    context.fill();
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use graphics::*;
 use web_sys::wasm_bindgen::JsCast;
 
 pub fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    log::debug!("Hello, world!");
+  wasm_logger::init(wasm_logger::Config::default());
+  log::debug!("Hello, world!");
 
-    if let Some(canvas) = get_canvas_2d("canvas") {
-        draw_triangle(canvas);
-    }
+  if let Some(canvas) = get_canvas_2d("canvas") {
+    let points = [
+      Point::new(200.0, 0.0),
+      Point::new(0.0, 400.0),
+      Point::new(400.0, 400.0),
+    ];
+    draw_triangle(&canvas, &points, false);
+  }
 }
