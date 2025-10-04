1c5fea7fee9ef9df12b6c4cdd8ce276d49cc560c
Takashi Ando <dodo5522@gmail.com>
2025-10-04 11:09:00 +0900

    [state] Set last frame at falling

--- src/game/state_machine/state/knocked_out.rs
+++ src/game/state_machine/state/knocked_out.rs
@@ -3,12 +3,20 @@ use super::RedHatBoyState;
 #[derive(Copy, Clone)]
 pub struct KnockedOut;
 
+impl KnockedOut {
+  const FRAMES: u8 = 29;
+}
+
 impl RedHatBoyState<KnockedOut> {
   pub fn frame_name(&self) -> &str {
     "Dead"
   }
 
-  pub fn update(self) -> Self {
+  pub fn update(mut self) -> Self {
+    self.context = self
+      .context
+      .update(KnockedOut::FRAMES)
+      .force_frame(KnockedOut::FRAMES);
     self
   }
 }
